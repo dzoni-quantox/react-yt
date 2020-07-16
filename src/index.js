5ce416d7020953ea8078d70a0933e010bbbbd4f9
Nikola Zivkovic <nikola.zivkovic@quantox.com>
2020-07-16 13:24:54 +0200

    videos list passed to VideoList component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from "../components/search_bar";
+import VideoList from "../components/video_list";
 
 const API_KEY = 'AIzaSyD1f8fyWzoYMkOIIVX9mTT2XxkgNIq_9Ss';
 
@@ -18,6 +19,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoList videos={this.state.videos} />
             </div>
         )
     };
