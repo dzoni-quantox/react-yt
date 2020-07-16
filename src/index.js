c0fde52fc1779c6e4ceb3b3e54ed0893f6bc3678
Nikola Zivkovic <nikola.zivkovic@quantox.com>
2020-07-16 12:59:49 +0200

    App made from function to class component, yt search moved to constructor and now updates state

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from "../components/search_bar";
 
 const API_KEY = 'AIzaSyD1f8fyWzoYMkOIIVX9mTT2XxkgNIq_9Ss';
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
 
-const App = () => {
-    return(
-        <div>
-            <SearchBar/>
-        </div>
-    )
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => this.setState({videos}));
+    }
+
+    render () {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        )
+    };
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
