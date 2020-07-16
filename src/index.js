06149d0c82f25ab8642ec692c341c2371554ab0d
Nikola Zivkovic <nikola.zivkovic@quantox.com>
2020-07-16 14:56:03 +0200

    added video details function component that previews first video from response

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from "../components/search_bar";
 import VideoList from "../components/video_list";
+import VideoDetail from "../components/video_details";
 
 const API_KEY = 'AIzaSyD1f8fyWzoYMkOIIVX9mTT2XxkgNIq_9Ss';
 
@@ -10,7 +11,9 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: []
+        };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => this.setState({videos}));
     }
@@ -19,6 +22,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos} />
             </div>
         )
