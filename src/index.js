58a0ee02de1774a36d8a04e50c1df8b1b3b9dba6
Nikola Zivkovic <nikola.zivkovic@quantox.com>
2020-07-16 12:49:05 +0200

    added a test call to yt api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from "../components/search_bar";
 
 const API_KEY = 'AIzaSyD1f8fyWzoYMkOIIVX9mTT2XxkgNIq_9Ss';
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 
 const App = () => {
     return(
